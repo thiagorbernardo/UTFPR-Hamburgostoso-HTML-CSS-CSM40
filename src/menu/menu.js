12341c820f127e36387c6041c51bdaebbb591f38
Thiago Ramos <thiagorbernardo@hotmail.com>
2022-05-24 21:52:32 -0300

    fix: table

--- src/menu/menu.js
+++
@@ -1,8 +0,0 @@
-const generateMenu = async (products) => {
-    const menu = document.getElementById('menuOptions');
-    products.forEach(product => {
-        const li = document.createElement('li');
-        li.innerHTML = `<a>${product.nome}</a>`;
-        menu.appendChild(li);
-    });
-}
