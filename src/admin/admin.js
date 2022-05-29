913a0b24a01e44f8837902c644c82b2708fb551c
Thiago Ramos <thiagorbernardo@hotmail.com>
2022-05-29 17:46:28 -0300

    feat: list categories and products

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,30 +1,47 @@
 const generateCategories = async () => {
     getAllCategories().then(categories => {
         console.log(categories)
-        const menu = document.getElementById('categoriesList');
+        const menu = document.getElementById('categories');
+
         categories.forEach(category => {
-            console.log(category)
-            const li = document.createElement('li');
-            li.innerHTML = `<a>${category.nome}</a>`;
-            menu.appendChild(li);
+            const a = document.createElement('a');
+            a.className = "card";
+            a.href = "products.html?id=" + category.id;
+            a.innerHTML = `<h2>${category.nome}</h2>
+            <p>${category.id}</p>`;
+            menu.appendChild(a);
         });
     })
 }
 
-const generateProducts = async () => {
-    getAllProducts().then(products => {
+const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+});
+
+const generateProducts = async (id) => {
+    getProductsByCategory(id).then(products => {
         console.log(products)
-        const menu = document.getElementById('productsList');
+        const menu = document.getElementById('categories');
+
         products.forEach(product => {
-            console.log(product)
-            const li = document.createElement('li');
-            li.innerHTML = `<a>${product.nome}</a>`;
-            menu.appendChild(li);
+            const a = document.createElement('a');
+            a.className = "card";
+            a.href = "product.html?id=" + product.id;
+            a.innerHTML = `<h2>${product.nome}</h2>
+            <p>${product.descricao}</p>
+            <p>ID: ${product.id}</p>
+            <p>Codigo: ${product.codigo}</p>
+            <p>ID Categoria: ${product.categoria}</p>
+            <p>Preço: ${formatter.format(product.preco)}</p>
+            <p>Peso: ${product.peso} Kg</p>
+            `;
+            menu.appendChild(a);
         });
     })
 }
 
-const generateProductForm = async () => {
+const generateProductForm = async (id) => {
 
 }
 
