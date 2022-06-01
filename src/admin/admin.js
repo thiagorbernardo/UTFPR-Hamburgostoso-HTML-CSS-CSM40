e619d9b6acdb658f2dcbf5c63b83037146d7958a
Thiago Ramos <thiagorbernardo@hotmail.com>
2022-05-31 22:07:53 -0300

    fix: forms

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,3 +1,8 @@
+const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+});
+
 const generateCategories = async () => {
     getAllCategories().then(categories => {
         console.log(categories)
@@ -11,8 +16,13 @@ const generateCategories = async () => {
             <p>${category.id}</p>`;
             menu.appendChild(a);
         });
-    }).then(categories => {
-        console.log(categories)
+
+        return categories
+    });
+}
+
+const generateCategoriesForm = async () => {
+    getAllCategories().then(categories => {
         const categorias = document.getElementById('category_options')
         categories.forEach(category => {
             const a = document.createElement('option');
@@ -24,10 +34,24 @@ const generateCategories = async () => {
     })
 }
 
-const formatter = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-});
+const inserirCategoria = async () => {
+    elemento = document.getElementById('form_categoria').value;
+
+    insertCategory(document.getElementById('form_categoria').value).then(() => {
+        window.location.href = "index.html";
+    });
+}
+
+const excluirCategoria = async () => {
+    const products = document.getElementById('categories');
+    if (products.childNodes.length > 1) {
+        alert("Você não pode apagar uma categoria que ainda contenha produtos.")
+        return
+    }
+    deleteCategory(urlParams.get('id')).then(() => {
+        window.location.href = "index.html";
+    });
+}
 
 const generateProducts = async (id) => {
     getProductsByCategory(id).then(products => {
@@ -37,18 +61,19 @@ const generateProducts = async (id) => {
         products.forEach(product => {
             const a = document.createElement('a');
             a.className = "card";
-            a.href = `?id=${id}&product_id=${product.id}#updateProduto`;
+            a.href = `?id=${id}#updateProduto`;
             a.onclick = () => {
-
-                document.getElementById('form_code').placeholder = product.codigo;
-                document.getElementById('form_name').placeholder = product.nome;
-                document.getElementById('form_description').placeholder = product.descricao;
-                document.getElementById('form_price').placeholder = product.codigo;
-                document.getElementById('form_weight').placeholder = product.peso;
-                document.getElementById('form_category').placeholder = product.categoria;
-                
-                //document.getElementById('form_image').innerHTML = product.image;
-                
+                document.getElementById('selectedProduct').value = product.id;
+console.log(document.getElementById('selectedProduct'))
+console.log(document.getElementById('selectedProduct').value)
+                document.getElementById('form_code').value = product.codigo;
+                document.getElementById('form_name').value = product.nome;
+                document.getElementById('form_description').value = product.descricao;
+                document.getElementById('form_price').value = product.codigo;
+                document.getElementById('form_weight').value = product.peso;
+                document.getElementById('category_options').value = product.categoria;
+                //TODO: adicionar imagem
+                //document.getElementById('form_image').value = product.image;
             }
             a.innerHTML = `<h2>${product.nome}</h2>
             <img src="../img/avatar.png" alt="Avatar" style="width:50%">
@@ -64,30 +89,29 @@ const generateProducts = async (id) => {
     })
 }
 
-const inserirCategoria = async () => {
-    elemento = document.getElementById('form_categoria').value;
-
-    insertCategory(document.getElementById('form_categoria').value).then(() => {
-        window.location.href = "index.html";
-    });
-
-}
-
 const inserirProduto = async () => {
-    insertProduct(document.getElementById('form_name').value, document.getElementById('form_code').value, document.getElementById('form_description').value, document.getElementById('form_price').value, document.getElementById('form_weight').value, document.getElementById('category_options').value);
+    insertProduct(
+        document.getElementById('form_name').value,
+        document.getElementById('form_code').value,
+        document.getElementById('form_description').value,
+        document.getElementById('form_price').value,
+        document.getElementById('form_weight').value,
+        document.getElementById('category_options').value
+    ).then(() => {
+        window.location.href = window.location.href.replace(window.location.hash, '');
+    });
 }
 
 const atualizarProduto = async () => {
-    updateProduct(document.getElementById('form_name').value, document.getElementById('form_code').value, document.getElementById('form_description').value, document.getElementById('form_price').value, document.getElementById('form_weight').value, document.getElementById('category_options').value);
-}
-
-const excluirCategoria = async () => {
-    const products = document.getElementById('categories');
-    if (products.childNodes.length > 1) {
-        alert("Você não pode apagar uma categoria que ainda contenha produtos.")
-        return
-    }
-    deleteCategory(urlParams.get('id')).then(() => {
-        window.location.href = "index.html";
+    updateProduct(
+        document.getElementById('selectedProduct').value,
+        document.getElementById('form_name').value,
+        document.getElementById('form_code').value,
+        document.getElementById('form_description').value,
+        document.getElementById('form_price').value,
+        document.getElementById('form_weight').value,
+        document.getElementById('category_options').value
+    ).then(() => {
+        window.location.href = "products.html?id=" + urlParams.get('id');
     });
 }
