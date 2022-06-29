106b17ee973ae8d0b526bb1c03519244f4f91181
PEDRO SODRE <sodre2306@gmail.com>
2022-06-29 09:29:31 -0300

    feat: pedidos list, modal in cadastro-not working

--- pedidos/pedidos.js
+++ pedidos/pedidos.js
@@ -2,14 +2,12 @@ const generateOrders = async () => {
     getAllOrders().then(orders => {
         console.log(orders)
         const menu = document.getElementById('orders');
-
+        const list = document.createElement('ul');
         orders.forEach(order => {
-            const a = document.createElement('a');
-            a.className = "card";
-            a.href = `products.html?id=${order.id}&name=${order.nome}`;
-            a.innerHTML = `<h2>${order.nome}</h2>
-            <p>${order.id}</p>`;
-            menu.appendChild(a);
+            const p = document.createElement('li');
+            p.innerHTML = `<h2>${order.id}, ${order.nome}, ${order.time}</h2>`;
+    
+            list.appendChild(p);
         });
 
         return orders
