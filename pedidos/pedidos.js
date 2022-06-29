4a808433987a05899c705141c86b196f593ddbad
PEDRO SODRE <sodre2306@gmail.com>
2022-06-29 12:34:22 -0300

    update: pedidos

--- pedidos/pedidos.js
+++ pedidos/pedidos.js
@@ -2,11 +2,16 @@ const generateOrders = async () => {
     getAllOrders().then(orders => {
         console.log(orders)
         const menu = document.getElementById('orders');
-        const list = document.createElement('ul');
+        const list = document.createElement('ol');
         orders.forEach(order => {
             const p = document.createElement('li');
-            p.innerHTML = `<h2>${order.id}, ${order.nome}, ${order.time}</h2>`;
-    
+            p.innerHTML =
+                 `<ul>
+                    <li><h2>${order.id}, ${order.nome}, ${order.time}</h2></li> </br>
+                    <li><h2>Endereço: ${order.rua}, ${order.numero} - ${order.cep}, ${order.cidade}, ${order.uf}</h2></li>
+                
+                 </ul>`;
+            //Colocar a lista de produtos no pedido aqui
             list.appendChild(p);
         });
 
